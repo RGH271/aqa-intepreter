954569edd3e26679c6ea3e077b88c75321e9551c
rgh271 <rhorrocks1@outlook.com>
2025-05-21 09:38:56 +0100

    add a bunch of starting stuff

--- main.py
+++ main.py
@@ -1,6 +1,36 @@
-def main():
-    print("Hello from aqa-intepreter!")
+import sys
 
 
-if __name__ == "__main__":
-    main()
+# skip over comments
+def Look():
+    global pc
+    if source[pc] == "#":
+        while source[pc] != "\n" and source[pc] != "\0":
+            pc += 1
+    return source[pc]
+
+
+#take away and return current character
+def Take():
+    global pc
+    c = Look()
+    pc += 1
+    return c
+
+
+
+
+# use program counter like a cpu
+pc = 0
+
+if len(sys.argv) < 2:
+    print("USAGE: int.py <sourcefile>")
+    exit(1)
+try:
+    # take first argument after binary is run
+    f = open(sys.argv[1], "r")
+except:
+    print("ERROR: Can't find source file '" + sys.argv[1] + "'")
+    exit(1)
+source = f.read() + "\0"
+f.close()
